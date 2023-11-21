9c8b7e29da7ab61e885c73b87c5c8ead453aad4f
VijayMPatil <1397.patilvijay@gmail.com>
2023-11-21 15:23:42 +0530

    add new models

--- apps/vendor/admin.py
+++ apps/vendor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.vendor.models import Vendor, PurchaseOrder,HistoricalPerformance
 
 # Register your models here.
+admin.site.register(Vendor)
+admin.site.register(PurchaseOrder)
+admin.site.register(HistoricalPerformance)
--- apps/vendor/models.py
+++ apps/vendor/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vendor(models.Model):
+    """Create a vendor model"""
+    name = models.CharField(max_length=255,null=True,blank=True)
+    contact_details = models.TextField(max_length=200)
+    address = models.TextField(max_length=200)
+    vendor_code = models.CharField(max_length=50, unique=True)
+    on_time_delivery_rate = models.FloatField(default=0.0)
+    quality_rating_avg = models.FloatField(default=0.0)
+    average_response_time = models.FloatField(default=0.0)
+    fulfillment_rate = models.FloatField(default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class PurchaseOrder(models.Model):
+    """Create a purchase order model"""
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Canceled', 'Canceled'),
+    ]
+
+    po_number = models.CharField(max_length=255, unique=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    quality_rating = models.FloatField(default=0.0, null=True, blank=True)
+    issue_date = models.DateTimeField(auto_now_add=True)
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+
+class HistoricalPerformance(models.Model):
+    """Create a historical performance model"""
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    on_time_delivery_rate = models.FloatField(default=0.0)
+    quality_rating_avg = models.FloatField(default=0.0)
+    average_response_time = models.FloatField(default=0.0)
+    fulfillment_rate = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f"{self.vendor.name} - {self.date}"
